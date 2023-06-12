b3e32d9174a1446e8317f8def3c56cc4799057a6
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2023-06-12 19:38:25 +0200

    quick upd Time

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -7,12 +7,19 @@ using System;
 
 public class TimerScript : MonoBehaviour
 {
+
+    public float TimePassed = 0f;
+    static public bool IsTimerOn = false;
+
     public TimerScriptableObject timerSO;
     public TextMeshProUGUI timerSOText;
     public float timerSOfloat;
 
-    public static Action MinuteChanged; // sekundy
-    public static Action HourChanged;   // minuty
+    public Text TimerTxt;
+
+
+    public static Action MinuteChanged; // sekundy 
+    public static Action HourChanged;   // minuty 
 
     public static int Minute { get; private set; }
     public static int Hour { get; private set; }
@@ -22,22 +29,28 @@ public class TimerScript : MonoBehaviour
     void Start()
     {
         timerSO.timer = minuteRealTime;
+        IsTimerOn = true;
 
     }
 
 
     void Update()
     {
+        if (IsTimerOn)
 
-        timerSO.timer -= Time.deltaTime;
+
+            timerSO.timer -= Time.deltaTime;
 
         if (timerSO.timer <= 0)
         {
             Minute++;
             MinuteChanged?.Invoke();
 
+            updateTimer(TimePassed);
+        }
 
-            if (Minute >= 60)
+
+        if (Minute >= 60)
             {
                 Hour++;
                 Minute = 0;
@@ -57,6 +70,9 @@ public class TimerScript : MonoBehaviour
         HourChanged += UpdateTime;
 
     }
+
+   
+
 
     private void OnDisable()
     {
