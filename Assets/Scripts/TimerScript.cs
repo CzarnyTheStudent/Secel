d6d54fda15bc369bd7fa86495f75398178e9f24b
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2023-06-12 19:59:05 +0200

    Fix Time Game

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -2,86 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using System;
 
 public class TimerScript : MonoBehaviour
 {
-
-    public float TimePassed = 0f;
+    static public float TimePassed = 0f;
     static public bool IsTimerOn = false;
 
-    public TimerScriptableObject timerSO;
-    public TextMeshProUGUI timerSOText;
-    public float timerSOfloat;
-
-
-    private static Action MinuteChanged; // sekundy 
-    private static Action HourChanged;   // minuty 
-
-    private static int Minute { get; set; }
-    private static int Hour { get; set; }
-
-    public float minuteRealTime = 1f;
-
+    public Text TimerTxt;
+   
     void Start()
     {
-        timerSO.timer = minuteRealTime;
         IsTimerOn = true;
-
     }
-
 
     void Update()
     {
-
-
-        timerSO.timer -= Time.deltaTime;
-
-        if (timerSO.timer <= 0)
+        if(IsTimerOn)
         {
-            Minute++;
-            MinuteChanged?.Invoke();
-
-            
-        
-
-
-            if (Minute >= 60) 
-            {
-                Hour++;
-                Minute = 0;
-                HourChanged?.Invoke();
-
-            }
-
-            timerSO.timer = minuteRealTime;
+            updateTimer(TimePassed);
         }
-
     }
 
-    private void OnEnable()
+    void updateTimer(float currentTime)
     {
-
-        MinuteChanged += UpdateTime;
-        HourChanged += UpdateTime;
-
+        TimePassed += Time.deltaTime;
+        string s = currentTime.ToString("0");
+        
+        TimerTxt.text = "Time: " + s +" seconds";
     }
-
-   
-
-
-    private void OnDisable()
-    {
-        MinuteChanged -= UpdateTime;
-        HourChanged -= UpdateTime;
-    }
-
-    void UpdateTime()
-    {
-        timerSOText.text = $"{Hour:00}:{Minute:00} AM";
-    }
-
-
 
 }
